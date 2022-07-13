b9e9264480d01e794a0f572598c201310764fe95
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-07-13 08:39:06 +0200

    display image on mobile ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("salut toi")
+console.log(document.getElementById("thumbs"))
+
+let countImg= 1; 
+
+function displayImg(){
+    document.getElementById("next-img").addEventListener("click", function(event){
+        countImg++;
+        if (countImg == 6) countImg = 1;
+        document.getElementById("display-img").src = `img/canard-jaune-${countImg}-l.png`;
+        console.log(document.getElementById("display-img").src)
+    })
+    
+    document.getElementById("last-img").addEventListener("click", function(event){
+        countImg--;
+        if (countImg == 0) countImg = 5;
+        document.getElementById("display-img").src = `img/canard-jaune-${countImg}-l.png`;
+        console.log(document.getElementById("display-img").src)
+    })
+}
+
+displayImg();
+
+ 
