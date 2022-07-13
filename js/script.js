5d80a9ffd54b4b25bf0f2cef397165a866230ee5
Zakaria Saber <zakaria.saber@hotmail.fr>
2022-07-13 08:56:20 +0200

    hover displayImg desktop

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
-console.log(document.getElementById("thumbs"))
+const displayImg = document.getElementById("display-img"); 
 
+// DISPLAY IMG ON MOBILE //
 let countImg= 1; 
 
-function displayImg(){
+function displayLNImg(){
     document.getElementById("next-img").addEventListener("click", function(event){
         countImg++;
         if (countImg == 6) countImg = 1;
-        document.getElementById("display-img").src = `img/canard-jaune-${countImg}-l.png`;
-        console.log(document.getElementById("display-img").src)
+        displayImg.src = `img/canard-jaune-${countImg}-l.png`;
+        console.log(displayImg.src)
     })
     
     document.getElementById("last-img").addEventListener("click", function(event){
         countImg--;
         if (countImg == 0) countImg = 5;
-        document.getElementById("display-img").src = `img/canard-jaune-${countImg}-l.png`;
-        console.log(document.getElementById("display-img").src)
+        displayImg.src = `img/canard-jaune-${countImg}-l.png`;
+        console.log(displayImg.src)
     })
 }
 
-displayImg();
+displayLNImg();
 
+ // HOVER ON DESKTOP // 
  
+ function hoverDisplayImg(){
+     const thumbs = document.getElementById("thumbs");
+
+     thumbs.addEventListener("mouseover", function(event){
+        if (!event.target.classList.contains("thumbs-img")) return;
+        displayImg.src = event.target.getAttribute("src")
+     })
+ }
+
+ hoverDisplayImg();
